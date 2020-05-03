56201245b371b2297a77c91ffb6b92e5186edb95
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-03 21:57:54 +0530

    added models

--- api/drones/admin.py
+++ api/drones/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Drone, DroneCategory, Pilot,Competition
 # Register your models here.
+admin.site.register(Drone)
+admin.site.register(DroneCategory)
+admin.site.register(Pilot)
+admin.site.register(Competition)
--- api/drones/models.py
+++ api/drones/models.py
@@ -1,3 +1,61 @@
 from django.db import models
 
 # Create your models here.
+class  DroneCategory(models.Model):
+    name = models.CharField(max_length=50)
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
+
+class Drone(models.Model):
+    name = models.CharField(max_length = 50)
+    manufacturing_date = models.DateTimeField()
+    category = models.ForeignKey('DroneCategory',on_delete=models.CASCADE,related_name='drones')
+    inserted_timestamp = models.DateTimeField(auto_now_add=True)
+    has_it_competed = models.BooleanField(default=False)
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
+#auto_now-> for update auto_now_add-> for created date
+
+class Pilot(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = (
+    (MALE, 'Male'),
+    (FEMALE, 'Female'),
+    )
+    
+    name = models.CharField(max_length = 50,blank=False,default='Pilot')
+    gender = models.CharField(max_length = 2,choices =GENDER_CHOICES, default=MALE)
+    races_count = models.IntegerField(null=False, blank=False,default=0)
+    inserted_timestamp = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ('name',)
+    def __str__(self):
+        return self.name
+
+class Competition(models.Model):
+    """"The competition must be
+        related to an existing pilot and an
+        existing drone.Each
+        competition must include the pilot's
+        name that made the drone reach a
+        specific distance and the drone's
+        name."""
+    #1 pilot, drone can be in many competition
+    competition_name = models.CharField(max_length = 30)
+    pilot = models.ForeignKey('Pilot',related_name='competitions',on_delete=models.CASCADE)
+    drone = models.ForeignKey('Drone',on_delete=models.CASCADE)
+    distance_in_feet  =models.IntegerField(null=False, blank=False)
+    class Meta:
+        # Order by distance in descending order
+       ordering = ('-distance_in_feet',)
+    def __str__(self):
+        return self.competition_name
+
+
+
+
