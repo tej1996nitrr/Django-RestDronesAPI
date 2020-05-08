66bb8a219de2c3c87a63df0bce88dc6ad8a290e5
tejas1996nitrr <tejaskanikdaley1996@gmail.com>
2020-05-08 23:58:48 +0530

    test for existing drone category

--- api/drones/tests/tests.py
+++ api/drones/tests/tests.py
@@ -28,5 +28,17 @@ class TestDroneCategory(APITestCase):
         assert response.status_code == status.HTTP_201_CREATED
         assert DroneCategory.objects.count()==1
         assert DroneCategory.objects.get().name == new_drone_category_name
+    def test_post_existing_drone_category_name(self):
+        """
+        Ensure we cannot create a DroneCategory with an existing name
+        """
+        url = reverse(DroneCategoryList.name)
+        new_drone_category_name = 'Duplicated Copter'
+        data = {'name': new_drone_category_name}
+        response1 = self.post_drone_category(new_drone_category_name)
+        assert response1.status_code == status.HTTP_201_CREATED
+        response2 = self.post_drone_category(new_drone_category_name)
+        print(response2)
+        assert response2.status_code == status.HTTP_400_BAD_REQUEST
 
 
